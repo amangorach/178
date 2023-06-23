615a4d298c28aaaf02bece812236c587e11e6bbe
amangorach <91610050+amangorach@users.noreply.github.com>
2023-06-23 19:51:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+let latitude = 7.846464, longitude = 9.837773787;
+mapboxgl.accessToken = "pk.eyJ1IjoiYXBvb3J2ZWxvdXMiLCJhIjoiY2ttZnlyMDgzMzlwNTJ4a240cmEzcG0xNyJ9.-nSyL0Gy2nifDibXJg4fTA";
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [longitude, latitude],
+    zoom: 16
+});
+
+map.addControl(
+    new mapboxgl.GeolocateControl({
+        positionOptions: { enableHighAccuracy: true },
+        trackUserLocation: true
+    })
+);
+
+map.addControl(
+    new MapboxDirections({
+        accessToken: mapboxgl.accessToken
+    }),
+    'top-left'
+);
